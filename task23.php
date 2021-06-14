ca6d3db4c53fd42011453370e69fa77bd9ac59a9
miatto88 <77558705+miatto88@users.noreply.github.com>
2021-06-14 23:54:04 +0900

    Add files via upload

--- task23.php
+++ task23.php
@@ -20,7 +20,7 @@ fclose($fp);
 // //81
 // ｀｀｀http://localhost:1234｀｀｀
 // こちらのURLにアクセスできるようなビルトインウェブサーバーのコマンドを書いてください。
-
+exec("php -S localhost:1234");
 
 
 // //82
@@ -28,6 +28,9 @@ fclose($fp);
 // "/app/doc/test/sample/dev/test.php"
 // 期待値
 // test.php
+$directory = "/app/doc/test/sample/dev/test.php";
+
+echo dirname($directory) . PHP_EOL;
 
 
 // //83
@@ -35,28 +38,37 @@ fclose($fp);
 // "/app/doc/test/sample/dev/test.php"
 // 期待値
 // /app/doc/test/sample/dev
-
+echo basename($directory) . PHP_EOL;
 
 
 // //84
-// $array = array(
-//        "a" => 1,
-//        "b" => 2,
-//    );
+$array = array(
+       "a" => 1,
+       "b" => 2,
+   );
 // 上記の配列をjson化して出力してください。
 // またjson化したものをデコードしてください。
+$toJason = json_encode($array);
+print_r($toJason);
+echo PHP_EOL;
 
+$toArray = json_decode($toJason, true);
+print_r($toArray);
+echo PHP_EOL;
 
 
 // //85
 // eyJtZXNzYWdlIjoiQ29uZ3JhdHVsYXRpb25zISJ9
 // 上記は、base64でエンコードされた文字列です。
 // base64でデコードし、さらにjsonをデコードされた内容をvar_dump()で出力してください。
-
+$base64 = "eyJtZXNzYWdlIjoiQ29uZ3JhdHVsYXRpb25zISJ9";
+$decode = base64_decode($base64);
+var_dump($decode);
 
 
 // //86
 // "Hello World"をhash化して、出力してください。
-
+$hash = password_hash("Hello World", PASSWORD_DEFAULT);
+echo $hash;
 
 ?>
